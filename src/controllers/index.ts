6595415057003a107a8652b1ebc7938e7dcb5620
Eric Ramos <ericramosofficial18@gmail.com>
2022-10-30 20:27:53 +0800

    Create model, repository and controller for Actor

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
 export * from './ping.controller';
 export * from './user.controller';
 export * from './movie.controller';
+export * from './actor.controller';
--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,2 +1,3 @@
 export * from './user.repository';
 export * from './movie.repository';
+export * from './actor.repository';
